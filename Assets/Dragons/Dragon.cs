abe1d0d7ca7898a45933b2725ec8e6808095dce3
Lando Tanghe <lando.tanghe@gmail.com>
2018-01-20 13:36:22 +0100

    Reposition dragons

--- Assets/Dragons/Dragon.cs
+++ Assets/Dragons/Dragon.cs
@@ -5,6 +5,20 @@ namespace Assets.Dragons
 {
     public class Dragon : MonoBehaviour
     {
+        public int x = 0;
+        public int y = 0;
+
+        private int loop = 0;
+
+        public void FixedUpdate()
+        {
+            loop++;
+            if(loop == 10)
+            {
+                transform.position = new Vector3(transform.position.x + 1 / 8.0f, transform.position.y, transform.position.z);
+                Debug.Log("move");
+            }                
+        }
 
         public bool CanTurnRight(Board board)
         {
